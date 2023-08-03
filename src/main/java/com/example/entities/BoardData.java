2063e0f081d08a9ac960efbc1c66c63604327ee9
kjk23654 <kjk23654@naver.com>
2023-08-03 17:43:43 +0900

    BoardData 클래스 생성

---
+++ src/main/java/com/example/entities/BoardData.java
@@ -0,0 +1,26 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board_data")
+@Data
+public class BoardData {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; // 게시글 번호
+    @Column(nullable = false, length = 100)
+    private String subject; // 제목
+    @Lob
+    @Column(nullable = false)
+    private String contents; // 내용
+
+    @Column(columnDefinition = "NUMBER(4) NOT NULL")
+    private int viewCounts; // 조회수
+
+    private LocalDateTime regDt; // 등록시간
+    private LocalDateTime modDt; // 수정시간
+}
